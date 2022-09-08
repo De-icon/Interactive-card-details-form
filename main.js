5e140c6dab031b3c34d09d4f6466522bcd6c6552
isaac john <isaacjohn2028@gmail.com>
2022-09-08 20:37:19 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function formatCreditCard() {
+    var x = document.getElementById("credit-card");
+    var index = x.value.lastIndexOf('-');
+    var test = x.value.substr(index + 1);
+    if (test.length === 4) x.value = x.value + '-';
+}
