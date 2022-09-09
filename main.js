18d651fcda598d8cb02965da3fe1dfd6d99e2dc8
isaac john <isaacjohn2028@gmail.com>
2022-09-09 21:35:17 +0100

    new changes

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+
+
+
+const cardNum = document.querySelector("#cardNum")
+const cardName = document.querySelector("#cardName")
+const cardDate = document.querySelector("#cardDate")
+const month = document.querySelector("#month")
+const year = document.querySelector("#year")
+const name = document.querySelector("#name")
+const Month = document.querySelector("#Month")
+const Year = document.querySelector("#Year")
+const cvc = document.querySelector("#cvc")
+const submit = document.querySelector("#submit")
+
 function formatCreditCard() {
-    var x = document.getElementById("credit-card");
-    var index = x.value.lastIndexOf('-');
-    var test = x.value.substr(index + 1);
-    if (test.length === 4) x.value = x.value + '-';
+    let x = document.getElementById("credit_card");
+    let index = x.value.lastIndexOf(' ');
+    let test = x.value.substr(index + 1);
+    if (test.length === 4) x.value = x.value + ' ';
+    
 }
+submit.addEventListener('click', () => {
+    cardNum.innerHTML = test.innerHTML
+
+})
