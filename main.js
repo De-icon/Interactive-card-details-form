ef60fc987e9c4517a625e07079805e182785bc12
isaac john <isaacjohn2028@gmail.com>
2022-12-14 01:05:59 +0100

    it now responsive

--- main.js
+++ main.js
@@ -1,35 +1,82 @@
 
-const cardNum = document.querySelector("#cardNum")J
+const cardNum = document.querySelector("#cardNum")
 const cardDate = document.querySelector("#cardDate")
 const submit = document.querySelector("#submit")
+const thankYou = document.querySelector("#thankYou")
+const Continue = document.querySelector("#Continue")
+const form = document.querySelector("form")
+const nameValue = document.querySelector('#name');
+const monthValue = document.querySelector("#Month")
+const yearValue = document.querySelector("#Year")
+const cvcValue = document.querySelector("#Cvc")
+
+
+//? formating the credit_card 
 
 function formatCreditCard() {
     let x = document.getElementById("credit_card");
     let index = x.value.lastIndexOf(' ');
     let test = x.value.substr(index + 1);
     if (test.length === 4) x.value = x.value + ' ';
-
-
+    const cardValue = document.querySelector("#cardNum")
+    cardValue.innerHTML = test
     
 }
+
+//* this for the name input and also output>>>>>>>>>>>>>>>>
+
 function nameInput() {
-    const nameValue = document.querySelector('#name').value;
-    const cardName = document.querySelector("#cardName");
-    cardName.innerHTML = nameValue.innerHTML
+    var nameVal = nameValue.value
+    const cardName = document.querySelector("#cardName");  
+    cardName.innerHTML = nameVal
 }
 
+//* this for the month input and also output>>>>>>>>>>>>>>>>
+
+
 function monthInput() {
-    const monthValue = document.querySelector("#Month").value;
+    var monthVal = monthValue.value
     const month = document.querySelector("#month");
-    month.innerHTML = monthValue.innerHTML
+    month.innerHTML = monthVal
 }
+
+//* this for the year input and also output>>>>>>>>>>>>>>>>
+
+
 function yearInput() {
-    const yearValue = document.querySelector("#Year").value;
+    var yearVal = yearValue.value
     const year = document.querySelector("#year");
-    year.innerHTML = yearValue.innerHTML
+    year.innerHTML = yearVal
 }
+
+//* this for the cvc input and also output>>>>>>>>>>>>>>>>
+
+
 function cvcInput() {
-    const cvcValue = document.querySelector("#Cvc").value;
+    var cvcVal = cvcValue.value
     const cvc = document.querySelector("#cvc");
-    cvc.innerHTML = cvcValue.innerHTML
+    cvc.innerHTML = cvcVal
 }
+
+//! submit botton for the form
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+
+
+    thankYou.classList.add("display")
+    form.classList.add("noDisplay")
+})
+
+//! continue botton
+
+Continue.addEventListener('click', () => {
+
+    nameValue.value = ''
+    
+    monthValue.value = ''
+    yearValue.value = ''
+    cvcValue.value = ''
+    form.classList.remove("noDisplay")
+    thankYou.classList.remove("display")
+})
